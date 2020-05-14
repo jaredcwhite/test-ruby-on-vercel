532a9782ed065d13d520ab9d8ab2f72971f8b781
Jared White <jared@jaredwhite.com>
2020-05-14 15:29:17 -0700

    Test servlet syntax

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -8,6 +8,16 @@ const endpoint = "/api/whats-the-time"
 document.querySelector("#callapi").addEventListener("click", () => {
   axios.get(endpoint).then(response => {
     // handle success
-    alert(response.data)
+    document.querySelector("#results").textContent = response.data
+  })
+})
+
+document.querySelector("#callpost").addEventListener("click", () => {
+  console.info("posting!")
+  axios.post(endpoint).then(response => {
+    console.info("response", response)
+    // handle success
+    const jsondata = response.data
+    document.querySelector("#results").textContent = `body: ${jsondata.body}, version: ${jsondata.version}`
   })
 })
--- src/index.md
+++ src/index.md
@@ -7,3 +7,7 @@ layout: home
 Welcome to Bridgetown!
 
 <button id="callapi">Let's Call a Serverless Function!</button>
+
+<button id="callpost">Let's POST to a Function!</button>
+
+<div id="results"></div>
