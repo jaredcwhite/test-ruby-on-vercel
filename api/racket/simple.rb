07b794bafd6db186bb6dd0ceda1cb593fe28e528
Jared White <jared@jaredwhite.com>
2020-06-08 09:32:10 -0700

    utf8 charset

--- api/racket/simple.rb
+++ api/racket/simple.rb
@@ -4,7 +4,7 @@ require_relative "../../lib/shared_code"
 
 class PhaedraFunction < Phaedra::Base
   def get(params)
-    response["Content-Type"] = "text/html"
+    response["Content-Type"] = "text/html; charset=utf-8"
     "<p>This is interesting. 🙌 #{SharedCode.run_once} / #{Time.now}</p>"
   end
 end
