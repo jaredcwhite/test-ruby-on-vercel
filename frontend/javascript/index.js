2c009ec9ebf45fb4c3bb8ec1156b9285f431c6db
Jared White <jared@jaredwhite.com>
2020-06-08 09:37:02 -0700

    try JSON input

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -16,19 +16,19 @@ document.querySelector("#callpost").addEventListener("click", () => {
   console.info("posting!")
   const fd = new FormData()
   fd.append("input", "barz")
-  axios.post(endpoint, fd).then(response => {
-    console.info("response", response)
-    // handle success
-    const jsondata = response.data
-    document.querySelector("#results").textContent = `body: ${jsondata.body}, version: ${jsondata.version}`
-  })
-
-/*  axios.patch(endpoint, {input: "JSON Works!"}).then(response => {
+/*  axios.post(endpoint, fd).then(response => {
     console.info("response", response)
     // handle success
     const jsondata = response.data
     document.querySelector("#results").textContent = `body: ${jsondata.body}, version: ${jsondata.version}`
   }) */
+
+  axios.patch(endpoint, {input: "JSON Works!"}).then(response => {
+    console.info("response", response)
+    // handle success
+    const jsondata = response.data
+    document.querySelector("#results").textContent = `body: ${jsondata.body}, version: ${jsondata.version}`
+  })
 
 /*  axios.delete(endpoint, {params: {input: "JSON Works!"}}).then(response => {
     console.info("response", response)
