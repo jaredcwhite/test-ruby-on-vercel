c49f9a9dac0c714890c265e98111aaf5dba6aeca
Jared White <jared@jaredwhite.com>
2020-07-03 09:02:46 -0700

    wrangling with utf-8 again

--- api/simple.rb
+++ api/simple.rb
@@ -2,13 +2,13 @@ require_relative "../phaedra/initializers"
 
 class PhaedraFunction < Phaedra::Base
   def get(params)
-    response["Content-Type"] = "text/html"
-    "<p>This is interesting! #{Phaedra::Shared.start_time} / #{Time.now}</p>"
+    response["Content-Type"] = "text/html; charset=utf-8"
+    "<p>This is interesting! 😎 #{Phaedra::Shared.start_time} / #{Time.now}</p>"
   end
 
   def post(params)
     {
-      file_contents: params[:filedata]
+      file_contents_length: params[:filedata].length
     }
   end
 end
