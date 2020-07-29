1efabd800e1c0495f502418f161315dc863d939d
Jared White <jared@jaredwhite.com>
2020-07-28 20:39:40 -0700

    what if it's utf-8 ?

--- api/vhandler.rb
+++ api/vhandler.rb
@@ -1,4 +1,4 @@
 Handler = Proc.new do |req, res|
-  res["Content-Type"] = "application/octet-stream; charset=ASCII-8BIT"
+  res["Content-Type"] = "application/octet-stream; charset=utf-8"
   res.body = "\xFF".b
 end
