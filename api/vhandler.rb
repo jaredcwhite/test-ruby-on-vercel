6b777ef313edf796c99a292e74240e133498e6e5
Jared White <jared@jaredwhite.com>
2020-07-28 20:31:10 -0700

    try a charset

--- api/vhandler.rb
+++ api/vhandler.rb
@@ -1,4 +1,4 @@
 Handler = Proc.new do |req, res|
-  res["Content-Type"] = "application/octet-stream"
+  res["Content-Type"] = "application/octet-stream; charset=ASCII-8BIT"
   res.body = "\xFF".b
 end
