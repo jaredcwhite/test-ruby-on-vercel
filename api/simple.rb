c34d2e462999d8a0e1528387c15482a3faf5bcd3
Jared White <jared@jaredwhite.com>
2020-07-28 20:11:36 -0700

    test binary data output

--- api/simple.rb
+++ api/simple.rb
@@ -2,8 +2,9 @@ require_relative "../phaedra/initializers"
 
 class PhaedraFunction < Phaedra::Base
   def get(params)
-    response["Content-Type"] = "text/html"
-    "<p>This is interesting! #{Phaedra.start_time} / #{Time.now}</p>"
+    "\xFF".b
+#    response["Content-Type"] = "text/html"
+#    "<p>This is interesting! #{Phaedra.start_time} / #{Time.now}</p>"
   end
 end
 
